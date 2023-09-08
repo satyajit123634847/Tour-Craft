52a184027881b6a565ccd24d80dee82bc7295078
sattttyyyya <satyajitvarpe45@gmail.com>
2023-09-08 09:37:52 +0530

    s

--- helper/emailHelper.js
+++ helper/emailHelper.js
@@ -9,13 +9,13 @@ exports.sendmail = (to, cc, subject, text, attachment, url) => {
         port: 465,
         secure: true, // use SSL
         auth: {
-            user: 'parthasarathy.ranganathan@airliquide.com',
-            pass: 'wyoifhtonaotimhp'
+            user: 'cryolorindia@gmail.com',
+            pass: 'tcpqrgvknqcjznoj'
         }
     })
 
     var mail = {
-        from: 'parthasarathy.ranganathan@airliquide.com',
+        from: 'cryolorindia@gmail.com',
         to: to,
         subject: subject,
         html: text,
@@ -31,49 +31,19 @@ exports.sendmail = (to, cc, subject, text, attachment, url) => {
     });
 }
 
-exports.sendmail1 = (to, cc, subject, html, attachments,from,app_password) => {
-    const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true, // use SSL
-        auth: {
-            user: from,
-            pass: app_password
-        }
-    });
-
-    const mailOptions = {
-        from: from,
-        to: to,
-        cc: cc,
-        subject: subject,
-        html: html,
-        attachments: attachments
-    };
-
-    transporter.sendMail(mailOptions, function (error, info) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log(info);
-        }
-    });
-};
-
-
-// exports.sendmail1 = (to, cc, subject, html, attachments) => {
+// exports.sendmail1 = (to, cc, subject, html, attachments,from,app_password) => {
 //     const transporter = nodemailer.createTransport({
 //         host: 'smtp.gmail.com',
 //         port: 465,
 //         secure: true, // use SSL
 //         auth: {
-//             user: 'parthasarathy.ranganathan@airliquide.com',
-//             pass: 'wyoifhtonaotimhp'
+//             user: from,
+//             pass: app_password
 //         }
 //     });
 
 //     const mailOptions = {
-//         from: 'parthasarathy.ranganathan@airliquide.com',
+//         from: from,
 //         to: to,
 //         cc: cc,
 //         subject: subject,
@@ -89,6 +59,36 @@ exports.sendmail1 = (to, cc, subject, html, attachments,from,app_password) => {
 //         }
 //     });
 // };
+
+
+exports.sendmail1 = (to, cc, subject, html, attachments) => {
+    const transporter = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        port: 465,
+        secure: true, // use SSL
+        auth: {
+            user: 'cryolorindia@gmail.com',
+            pass: 'tcpqrgvknqcjznoj'
+        }
+    });
+
+    const mailOptions = {
+        from: 'cryolorindia@gmail.com',
+        to: to,
+        cc: cc,
+        subject: subject,
+        html: html,
+        attachments: attachments
+    };
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log(info);
+        }
+    });
+};
 
 
 
